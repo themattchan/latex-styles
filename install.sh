d6984a7baba28d5dc66ec5ed17a3e0a382a93d55
Matthew Chan <matt@themattchan.com>
2017-01-26 06:59:58 +0800

    linux install

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-#! /usr/local/bin/zsh
+#! /bin/bash
 
-rsync -r ./. /Users/matt/Library/texmf/tex/latex/local
-rsync -r ./. /usr/local/texlive/texmf-local/tex/latex/local
-sudo texhash
+LINUX_MYTEX=$(kpsewhich -var-value=TEXMFHOME)
+
+if [ "$(uname)" == "Darwin" ]; then
+    rsync -r ./. /Users/matt/Library/texmf/tex/latex/local
+    rsync -r ./. /usr/local/texlive/texmf-local/tex/latex/local
+    sudo texhash    
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    rsync -r ./. $LINUX_MYTEX/tex/latex/local
+fi
